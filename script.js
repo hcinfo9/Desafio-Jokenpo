94385353a4d51c15d5271e0ac6e1ab25dcc60d30
hcinfo9 <hcinformatica9438@gmail.com>
2024-05-29 20:44:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,113 @@
+
+const imagem_humano = document.querySelector('.imagem-humano')
+const imagem_maquina = document.querySelector('.imagem-maquina')
+const containerButton = document.querySelector('.section-button')
+const containerResultado= document.querySelector('.container-resultado')
+const textResultado = document.querySelector('.resultado-titulo')
+const placarHumano = document.querySelector('.container-humano h2')
+const placarMaquina = document.querySelector('.container-maquina h2')
+ 
+let pontosMaquina=0
+let pontosHumano=0
+
+
+const jogadaHumana = escolha => { 
+
+
+    const jogadaMaquina = array => {
+        let escolha = array[Math.floor(Math.random() * array.length)]
+        return escolha
+    }
+
+    const escolhaMaquina=jogadaMaquina(['paper', 'rock', 'scissors'])
+   
+    const mostrarAnimacao = (elementoHumano, elementoMaquina) => {
+
+        elementoHumano.setAttribute('src','./assets/Human_Animation/up.png')
+        elementoMaquina.setAttribute('src','./assets/Machine_Animation/up.png')
+
+        setTimeout(()=>{
+            elementoHumano.setAttribute('src','./assets/Human_Animation/down.png')
+            
+            elementoMaquina.setAttribute('src','./assets/Machine_Animation/down.png')
+        },500)
+       
+    }
+  
+    
+    const repetir = (totalRepicoes, intervalo) =>{
+        let repetir =0
+
+        function executar(){
+            mostrarAnimacao(imagem_humano,imagem_maquina);
+            repetir++
+
+            if (repetir < totalRepicoes){
+                setTimeout(executar, intervalo)
+            }
+            else{
+                Timeout()
+            }
+        }
+        executar()
+    }
+    repetir(3, 1000)
+
+
+    const Timeout = () =>{
+        setTimeout (() => {
+            iniciarJogo(escolha, escolhaMaquina)
+        },1000)
+    
+        setTimeout (() => {
+            containerButton.style.display = 'none'
+        },1500)
+    
+         setTimeout (() => {
+            containerResultado.style.display = 'flex'
+         },1500)
+
+         setTimeout(()=>{
+            imagem_humano.setAttribute('src',`./assets/Human_Animation/${escolha}.png`)
+            imagem_maquina.setAttribute('src',`./assets/Machine_Animation/${escolhaMaquina}.png`)
+        },1000)
+    } 
+    
+}
+
+const iniciarJogo= (escolhaHumana, escolhaMaquina) => {
+
+    if (escolhaHumana === escolhaMaquina){
+        textResultado.innerHTML='Deu Empate!'
+       
+    }
+
+    else if(escolhaHumana==='paper' && escolhaMaquina==='rock' || escolhaHumana==='rock' && escolhaMaquina ==='scissors' || escolhaHumana==='scissors' && escolhaMaquina ==='paper'){
+        pontosHumano++
+        placarHumano.innerHTML=pontosHumano
+        textResultado.innerHTML='Você Ganhou!'
+    }
+    else{
+        pontosMaquina++
+        placarMaquina.innerHTML=pontosMaquina
+        textResultado.innerHTML='Você Perdeu!'
+    }
+}
+
+const proximaJogada = () =>{
+    imagem_humano.setAttribute('src', './assets/screen initial/human_Arm.png')
+    imagem_maquina.setAttribute('src', './assets/screen initial/AI Arm.png')
+    containerButton.style.display = 'flex'
+    containerResultado.style.display='none'
+}
+
+const resetarPontos = () =>{
+    imagem_humano.setAttribute('src', './assets/screen initial/human_Arm.png')
+    imagem_maquina.setAttribute('src', './assets/screen initial/AI Arm.png')
+    pontosHumano=0
+    pontosMaquina=0
+    placarHumano.innerHTML='0'
+    placarMaquina.innerHTML='0'
+    containerButton.style.display='flex'
+    containerResultado.style.display='none'
+}
